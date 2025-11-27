3aa9157ff365c4908a9495d4bbb32d8d5a57b578
Pedro <167371137+PedroFPer@users.noreply.github.com>
2025-11-27 09:06:13 -0300

    DriverTripManifest
    -Adicionado biblioteca actionsheet-react
    -Adicionado ActionSheet no projeto
    -Atualizado a estilização e a estrutura do Action Sheet

--- expresso_unix/src/ui/pages/driver/driver-trip-manifest/Components/PassengerManifest.jsx
+++ expresso_unix/src/ui/pages/driver/driver-trip-manifest/Components/PassengerManifest.jsx
@@ -1,38 +1,42 @@
-import { useState } from "react";
-import qrCodeDefault from "../../../../../assets/qr-code-default.png"
+import React, { useRef, useState } from "react";
+import ActionSheet from "actionsheet-react";
+import qrCodeDefault from "../../../../../assets/qr-code-default.png";
 import { formatUtils } from "../../../../../infrastructure/utils/formatUtils";
-import "../styles/PassengerManifest.css"
+import "../styles/PassengerManifest.css";
 
 export default function PassengerManifest({ passengerList, setPassengerList }) {
+
+    const sheetRef = useRef(null);
 
     const [search, setSearch] = useState("");
 
     const sortedList = [...passengerList].sort((a, b) => a.name.localeCompare(b.name));
 
     const filteredList = sortedList.filter(passenger =>
-        passenger.name.toLowerCase().startsWith(formatUtils.normalizeForSearch(search))
+        formatUtils.normalizeForSearch(passenger.name)
+            .startsWith(formatUtils.normalizeForSearch(search))
     );
 
     const handleTogglePresence = (id) => {
         setPassengerList(prev =>
             prev.map(item => {
                 if (item.id === id) {
-
                     return {
                         ...item,
                         isPresent: item.isPresent === "Ausente" ? "Presente" : "Ausente"
                     };
-
                 }
-
                 return item;
             })
         );
+    };
 
+    const handleClose = (e) => {
+        sheetRef.current?.close();
     };
 
     return (
-        <form id="passenger-manifest" >
+        <form id="passenger-manifest">
             <header>
                 <input
                     id="form-input-manifest"
@@ -43,21 +47,48 @@ export default function PassengerManifest({ passengerList, setPassengerList }) {
                 />
             </header>
 
-            <ul id="passenger-list" >
-                {filteredList.map((passenger) => {
-                    return (
-                        <li className="passenger-item" key={passenger.id}>
-
-                            <input type="checkbox" onClick={() => handleTogglePresence(passenger.id)} />
-                            <strong>{passenger.name}</strong>
-                            <figure><img src={qrCodeDefault} alt="Escanear qrcode" /></figure>
-
-                        </li>
-
-                    )
-                })}
+            <ul id="passenger-list">
+                {filteredList.map((passenger) => (
+                    <li className="passenger-item" key={passenger.id}>
+                        <input type="checkbox" onClick={() => handleTogglePresence(passenger.id)} />
+                        <strong>{passenger.name}</strong>
+                        <figure><img src={qrCodeDefault} alt="Escanear qrcode" /></figure>
+                    </li>
+                ))}
             </ul>
-        </form>
 
-    )
+            <button type="button" onClick={() => sheetRef.current?.open()} className="btn btn-primary btn-custom">Finalizar Chamada</button>
+
+
+            <ActionSheet
+                ref={sheetRef}
+                sheetStyle={{
+                    backgroundColor: '#F9F9F9',   
+                    borderTopLeftRadius: '16px',
+                    borderTopRightRadius: '16px',
+                    padding: '1em',
+
+                    display: 'grid',        
+                    justifyItems: 'center',      
+                           
+                }}
+                maskStyle={{
+                    backgroundColor: 'rgba(0,0,0,0.5)'
+                }}>
+                <aside id="action-sheet-manifest">
+
+                    <div id="confirm-finish-manifest-message">
+                        <p>Ainda há passageiros que não confirmaram presença.</p>
+                        <p>Deseja finalizar a chamada mesmo assim?</p>
+                    </div>
+
+                    <div id="confirm-finish-manifest-group-btn">
+                        <button id="btn_finish_manifest" className="btn" >Finalizar</button>
+                        <button id="btn-cancel-finish-manifest" className="btn" onClick={handleClose}>Cancelar</button>
+                    </div>
+
+                </aside>
+            </ActionSheet>
+        </form>
+    );
 }
