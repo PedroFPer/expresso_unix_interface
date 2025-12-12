fd64d2c47b1e15450a31bdc8d9d7de790f0ce10b
Daniel <166452209+star-Daniels@users.noreply.github.com>
2025-12-12 12:17:49 -0300

    Responsividade

--- expresso_unix/src/ui/pages/administrator/admin-manage-employees/components/ModelHeaderAddEmployees.jsx
+++ expresso_unix/src/ui/pages/administrator/admin-manage-employees/components/ModelHeaderAddEmployees.jsx
@@ -4,7 +4,7 @@ export default function ModelHeaderAddEmployees({ setActiveTab }) {
     return (
 
         <header id="model-header-add-employees">
-            <h2>Adicionar Viagem</h2>
+            <h2>Adicionar Funcionário</h2>
             <nav id="nav-tab-add-employees">
                 <button onClick={() => setActiveTab("form")}>Formulário</button>
                 <button onClick={() => setActiveTab("preview")}>Prévia</button>
