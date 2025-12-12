c6099d7a266352326375e95ce18e4aed10966c01
Pedro <167371137+PedroFPer@users.noreply.github.com>
2025-12-12 09:15:56 -0300

    Update

--- expresso_unix/src/ui/pages/administrator/admin-manage-employees/AdminManageEmployees.jsx
+++ expresso_unix/src/ui/pages/administrator/admin-manage-employees/AdminManageEmployees.jsx
@@ -1,44 +1,76 @@
 import { useState } from "react";
-import SideBar from "../../../common-components/components/SideBar"
+import SideBar from "../../../common-components/components/SideBar";
 import HeaderManagerEmployees from "./components/HeaderManagerEmployees";
 import EmployeesPanel from "./components/EmployeesPanel";
 import ModalAddEmployees from "./components/ModalAddEmployees";
-import "./styles/AdminManageEmployees.css"
+import ModalEditEmployees from "./components/ModalEditEmployees";
+import "./styles/AdminManageEmployees.css";
 
 export default function AdminManageEmployees() {
 
     const [sideBarIsOpen, setSideBarIsOpen] = useState(true);
-    const [openModal, setOpenModal] = useState(false);
-    const [overlayActive, setOverlayActive] = useState(false)
+    const [openAddModal, setOpenAddModal] = useState(false);
+    const [openEditModal, setOpenEditModal] = useState(false);
+    const [overlayActive, setOverlayActive] = useState(false);
 
+    const [selectedEmployee, setSelectedEmployee] = useState(null);
 
-    const handleToggleModal = () => {
-        setOpenModal(prev => !prev);
+    const handleToggleAddModal = () => {
+        setOpenAddModal(prev => !prev);
         setOverlayActive(prev => !prev);
     };
 
+    const handleToggleEditModal = (employee) => {
+        console.log(employee)
+        setSelectedEmployee(employee);
+        setOpenEditModal(prev => !prev);
+        setOverlayActive(prev => !prev);
+    };
 
     return (
-
-        <div id="admin-manage-employees"
-            className={sideBarIsOpen ? "manage-employees-sidebar-open" : "manage-employees-sidebar-closed"}>
+        <div
+            id="admin-manage-employees"
+            className={
+                sideBarIsOpen
+                    ? "manage-employees-sidebar-open"
+                    : "manage-employees-sidebar-closed"
+            }
+        >
             <SideBar
                 sideBarIsOpen={sideBarIsOpen}
-                setSideBarIsOpen={setSideBarIsOpen}></SideBar>
+                setSideBarIsOpen={setSideBarIsOpen}
+            />
+
             <div id="main-manage-employees">
-                <HeaderManagerEmployees ></HeaderManagerEmployees>
-                <EmployeesPanel handleToggleModal={handleToggleModal}></EmployeesPanel>
-                <ModalAddEmployees openModal={openModal}></ModalAddEmployees>
+                <HeaderManagerEmployees />
+
+                <EmployeesPanel
+                    handleToggleAddModal={handleToggleAddModal}
+                    handleToggleEditModal={handleToggleEditModal}
+                />
+
+                <ModalAddEmployees
+                    openAddModal={openAddModal}
+                    handleToggleAddModal={handleToggleAddModal}
+                />
+
+                <ModalEditEmployees
+                    openEditModal={openEditModal}
+                    handleToggleEditModal={handleToggleEditModal}
+                    selectedEmployee={selectedEmployee}
+                />
             </div>
 
             {overlayActive && (
-                <div className="overlay-modal-manage-employees"
-                    onClick={handleToggleModal}></div>
+                <div
+                    className="overlay-modal-manage-employees"
+                    onClick={() => {
+                        setOpenAddModal(false);
+                        setOpenEditModal(false);
+                        setOverlayActive(false);
+                    }}
+                ></div>
             )}
         </div>
-
-
     );
 }
-
-
--- expresso_unix/src/ui/pages/administrator/admin-manage-employees/components/ModalAddEmployees.jsx
+++ expresso_unix/src/ui/pages/administrator/admin-manage-employees/components/ModalAddEmployees.jsx
@@ -1,20 +1,108 @@
+import { useState, useContext } from "react";
 import ModalHeaderAddEmployees from "./ModalHeaderAddEmployees"
 import ModalFormAddEmployees from "./ModalFormAddEmployees"
+import { AdminContext } from "../../../../../infrastructure/context/AdminProvider";
+import { validationsUtils } from "../../../../../infrastructure/utils/validationsUtils";
 import "../styles/ModalAddEmployees.css"
 
-export default function ModalAddEmployees({ openModal }) {
+export default function ModalAddEmployees({ openAddModal, handleToggleAddModal }) {
+    const { employees, setEmployees } = useContext(AdminContext);
+
+    const [employee, setEmployee] = useState({
+        name: "",
+        email: "",
+        role: "",
+        password: "",
+        status: ""
+    });
+
+    const emptyEmployee = {
+        name: "",
+        email: "",
+        role: "",
+        password: "",
+        status: ""
+    };
+
+    const [errors, setErrors] = useState({
+        name: "",
+        email: "",
+        role: ""
+    });
+
+    const validationMap = {
+        name: "name",
+        email: "email",
+        role: "select"
+    };
+
+    const handleInputChange = (attribute, value) => {
+        const field = validationMap[attribute];
+
+
+        setEmployee(prev => ({
+            ...prev,
+            [attribute]: value
+        }));
+
+        const validation = validationsUtils.validateField(field, value);
+
+        console.log(validation.message);
+
+        setErrors(prev => ({
+            ...prev,
+            [attribute]: validation.valid ? "" : validation.message
+        }));
+    };
+
+    const registerEmployee = () => {
+        const newErrors = {};
+
+        const finalEmployee = {
+            ...employee,
+            id: crypto.randomUUID(),
+            password: "Senha@123",
+            status: "Ativo"
+        };
+
+
+        Object.entries(validationMap).forEach(([attribute, field]) => {
+            const validation =
+                validationsUtils.validateField(field, finalEmployee[attribute]) ||
+                { valid: false, message: "Campo inválido" };
+
+            if (!validation.valid) {
+                newErrors[attribute] = validation.message;
+            }
+        });
+
+        setErrors(newErrors);
+
+        if (Object.keys(newErrors).length > 0) return;
+
+        setEmployees(prev => [...prev, finalEmployee]);
+
+        window.alert("Usuário cadastrado!");
+
+        setEmployee(emptyEmployee);
+        handleToggleAddModal();
+    };
 
     return (
-        <aside className={`modal-add-employees ${openModal ? "modal-add-employees-show" : "modal-add-employees-hidden"}`}>
-            <ModalHeaderAddEmployees ></ModalHeaderAddEmployees>
+        <aside className={`modal-add-employees ${openAddModal ? "modal-add-employees-show" : "modal-add-employees-hidden"}`}>
+            <ModalHeaderAddEmployees />
 
-           <ModalFormAddEmployees></ModalFormAddEmployees>
+            <ModalFormAddEmployees
+                handleInputChange={handleInputChange}
+                registerEmployee={registerEmployee}
+                employee={employee}
+                errors={errors}
+            />
 
             <div id="group-button-add-employees">
-                <button id="cancel-button-add-employees">Cancelar</button>
-                <button id="continue-button-add-employees">Continuar</button>
-        
+                <button id="cancel-button-add-employees" onClick={handleToggleAddModal}>Cancelar</button>
+                <button id="continue-button-add-employees" onClick={registerEmployee}>Continuar</button>
             </div>
         </aside>
-    )
+    );
 }
