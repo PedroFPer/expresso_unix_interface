d947e634c8bb2a152eaa41d7615592848a1575b1
Pedro <167371137+PedroFPer@users.noreply.github.com>
2025-12-13 15:45:22 -0300

    Update

--- expresso_unix/src/ui/pages/administrator/admin-manage-employees/components/HeaderManagerEmployees.jsx
+++
@@ -1,9 +0,0 @@
-import "../styles/HeaderManagerEmployees.css"
-
-export default function HeaderManagerEmployees(){
-    return(
-        <div id="header-manager-employees">
-            <h1>Gerenciar Funcionarios</h1>
-        </div>
-    )
-}
--- expresso_unix/src/ui/pages/driver/driver-login/Components/HeaderDriverLogin.jsx
+++
@@ -1,8 +0,0 @@
-import logoExpressoUnix from "../../../../../assets/logo-expresso-unix.png"
-import "../styles/HeaderDriverLogin.css"
-
-export default function HeaderDriverLogin(){
-    return(
-        <figure id="header-driver-login"><img src={logoExpressoUnix} alt="Logo Expresso Unix" /></figure>
-    )
-}
